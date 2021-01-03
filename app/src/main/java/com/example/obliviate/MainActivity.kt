484d98625acb867c2a637e86016cea83ddfbe263
sangwook <9967han@naver.com>
2021-01-04 02:33:13 +0900

    print text by day_of_year

--- app/src/main/java/com/example/obliviate/MainActivity.kt
+++ app/src/main/java/com/example/obliviate/MainActivity.kt
@@ -5,6 +5,11 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.obliviate.databinding.ActivityMainBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,21 +19,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+        val curTime = System.currentTimeMillis()
         setContentView(view)
         db.collection("Text")
             .get()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    val today : String? = getTodayNum()
                     for (document in task.result!!) {
-                        binding.mainTextview.text = unescape(document.data["string"].toString())
+                        if(document.data["idx"].toString() == today){
+                            binding.mainTextview.text = unescape(document.data["string"].toString())
+                            break
+                        }
                     }
                 } else {
-                    Log.d("aerror", "Error getting documents.", task.exception)
+                    Log.d("error", "Error getting documents.", task.exception)
                 }
             }
     }
 
     private fun unescape(description: String): String? {
         return description.replace("\\\\n".toRegex(), "\\\n")
+    }
+
+    private fun getTodayNum() : String? {
+        var cal = Calendar.getInstance()
+        var day = cal.get(Calendar.DAY_OF_YEAR).toString()
+        return day
     }
 }
