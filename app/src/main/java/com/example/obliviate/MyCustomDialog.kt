41a9efaa872b38ed5d8ffc156cb100726637d643
sangwook <9967han@naver.com>
2021-01-20 01:29:44 +0900

    use colorPicker library

--- app/src/main/java/com/example/obliviate/MyCustomDialog.kt
+++ app/src/main/java/com/example/obliviate/MyCustomDialog.kt
@@ -4,10 +4,13 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.example.obliviate.databinding.CustomDialogBinding
+import com.github.dhaval2404.colorpicker.ColorPickerDialog
+import com.github.dhaval2404.colorpicker.model.ColorShape
+import com.github.dhaval2404.colorpicker.util.ColorUtil.parseColor
 
 class MyCustomDialog(context: Context, myCustomDialogInterface: MyCustomDialogInterface): Dialog(context), View.OnClickListener {
 
@@ -27,6 +30,8 @@ class MyCustomDialog(context: Context, myCustomDialogInterface: MyCustomDialogIn
         //배경 투명
         window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         binding?.dialogWidgetRegisterButton?.setOnClickListener(this)
+        binding?.dialogColorBtn1?.setOnClickListener(this)
+        binding?.dialogColorBtn2?.setOnClickListener(this)
     }
 
     override fun onClick(view : View?) {
@@ -34,6 +39,25 @@ class MyCustomDialog(context: Context, myCustomDialogInterface: MyCustomDialogIn
             binding?.dialogWidgetRegisterButton -> {
                 this.myCustomDialogInterface?.onRegisterBtnClicked()
             }
+
+            binding?.dialogColorBtn1 -> {
+                binding?.dialogText?.setBackgroundResource(R.drawable.dialog1)
+            }
+
+            binding?.dialogColorBtn2 -> {
+                ColorPickerDialog
+                    .Builder(context)        				// Pass Activity Instance
+                    .setTitle("배경 색 설정")  // Default ColorShape.CIRCLE
+                    .setDefaultColor(parseColor("#ffffff"))            	// Default "Choose Color"
+                    .setColorShape(ColorShape.CIRCLE)   // Default ColorShape.CIRCLE
+                    .setColorListener { color, colorHex ->
+                        // Handle Color Selection
+                    }
+                    .show()
+            }
+
         }
     }
 }
+
+
